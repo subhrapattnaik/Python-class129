6b2a4714d576f8521a2f03e84bc410862566110e
Subhra Pattnaik <50340351+subhrapattnaik@users.noreply.github.com>
2021-10-21 11:40:38 +0530

    Update SortColumn.py

--- SortColumn.py
+++ SortColumn.py
@@ -2,7 +2,7 @@ import csv
 import pandas as pd 
 
 data = []
-with open("D:/Aditya/Whitehat Jr/Module 3/129/dataset_2.csv","r") as f :
+with open("D:/WhitehatJr/Module3/129/dataset_2.csv","r") as f :
     csvreader= csv.reader(f)
     for row in csvreader:
         data.append(row)
@@ -19,7 +19,7 @@ for data_point in planet_data :
 #Sorting Planet names in alphabetical order 
 
 planet_data.sort(key=lambda planet_data:planet_data[2])
-with open("D:/Aditya/Whitehat Jr/Module 3/129/dataset_2_sorted.csv","a+") as f :
+with open("D:/WhitehatJr/Module3/129/dataset_2_sorted.csv","a+") as f :
     csvwriter = csv.writer(f)
     csvwriter.writerow(headers)
     csvwriter.writerows(planet_data)
